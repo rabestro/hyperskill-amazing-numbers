0deba5a3edb49880fe9fa97c67526eeaa67e11bb
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-04-18 20:43:23 +0300

    Added forever while loop

--- Amazing Numbers I/task4/src/numbers/Main.java
+++ Amazing Numbers I/task4/src/numbers/Main.java
@@ -6,14 +6,19 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Enter a natural number:");
-        NumberProperties.number = scanner.nextLong();
+        while (true) {
+            System.out.println("Enter a natural number:");
+            NumberProperties.number = scanner.nextLong();
+            if (NumberProperties.number == 0) {
+                break;
+            }
+            if (NumberProperties.isNatural()) {
+                System.out.printf("Properties of %,d%n", NumberProperties.number);
+                NumberProperties.stream().forEach(System.out::print);
+            } else {
+                System.out.println("This number is not natural!");
+            }
 
-        if (NumberProperties.isNatural()) {
-            System.out.printf("Properties of %,d%n", NumberProperties.number);
-            NumberProperties.stream().forEach(System.out::print);
-        } else {
-            System.out.println("This number is not natural!");
         }
     }
 
