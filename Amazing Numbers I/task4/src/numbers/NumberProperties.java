ba5bdfd917fa8d4c9b04b340d53bae5c39d1e155
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-04-18 20:35:57 +0300

    Added implementation for stage 4

---
+++ Amazing Numbers I/task4/src/numbers/NumberProperties.java
@@ -0,0 +1,37 @@
+package numbers;
+
+import java.util.Arrays;
+import java.util.function.LongFunction;
+import java.util.stream.Stream;
+
+import static java.lang.Character.getNumericValue;
+
+public enum NumberProperties {
+    PARITY(number -> number % 2 == 0 ? "even" : "odd"),
+    BUZZ(number -> number % 7 == 0 || number % 10 == 7),
+    DUCK(number -> String.valueOf(number).indexOf('0') != -1),
+    GAPFUL(number -> number > 100
+            && number % (getNumericValue(String.valueOf(number).charAt(0)) * 10L + number % 10) == 0);
+
+    public static String FORMAT = "%12s: %s%n";
+    public static long number = 0;
+
+    private final LongFunction<Object> calculateProperty;
+
+    NumberProperties(LongFunction<Object> calculateProperty) {
+        this.calculateProperty = calculateProperty;
+    }
+
+    public static boolean isNatural() {
+        return number > 0;
+    }
+
+    public static Stream<NumberProperties> stream() {
+        return Arrays.stream(NumberProperties.values());
+    }
+
+    @Override
+    public String toString() {
+        return String.format(FORMAT, name().toLowerCase(), calculateProperty.apply(number));
+    }
+}
