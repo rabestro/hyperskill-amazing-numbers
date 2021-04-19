ac3c98d2726be39e2780023ec0e204e0256cf74f
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-04-19 10:06:56 +0300

    Add implementation for stage 5

--- Amazing Numbers I/task5/src/numbers/Main.java
+++ Amazing Numbers I/task5/src/numbers/Main.java
@@ -1,25 +1,37 @@
 package numbers;
 
 import java.util.Scanner;
+import java.util.stream.LongStream;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         while (true) {
-            System.out.println("Enter a natural number:");
-            NumberProperties.number = scanner.nextLong();
-            if (NumberProperties.number == 0) {
+            System.out.printf("%nEnter a natural number: ");
+            final var data = scanner.nextLine().split(" ");
+            System.out.println();
+            final var start = Long.parseLong(data[0]);
+            if (start == 0) {
                 break;
             }
-            if (NumberProperties.isNatural()) {
-                System.out.printf("Properties of %,d%n", NumberProperties.number);
-                NumberProperties.stream().forEach(System.out::print);
-            } else {
+            if (start < 0) {
                 System.out.println("This number is not natural!");
             }
-
+            if (data.length == 1) {
+                System.out.print(NumberProperties.fullProperties(start));
+                continue;
+            }
+            final var count = Long.parseLong(data[1]);
+            if (count < 1) {
+                System.out.println("The count should be greater then zero.");
+                continue;
+            }
+            LongStream.range(start, start + count).forEach(Main::printProperties);
         }
     }
 
+    private static void printProperties(long number) {
+        System.out.printf("%,16d is %s%n", number, NumberProperties.shortProperties(number));
+    }
 }
