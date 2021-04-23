18d2fc0e0aed1ff601a3e4ab229b3ef29601807e
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-04-23 20:54:25 +0300

    Updated test 2

--- Amazing Numbers I/task2/test/NumbersTest.java
+++ Amazing Numbers I/task2/test/NumbersTest.java
@@ -1,204 +1,100 @@
 import org.hyperskill.hstest.dynamic.DynamicTest;
-import org.hyperskill.hstest.exception.outcomes.WrongAnswer;
 import org.hyperskill.hstest.stage.StageTest;
 import org.hyperskill.hstest.testcase.CheckResult;
-import org.hyperskill.hstest.testing.TestedProgram;
+import util.TextChecker;
 
-import java.text.MessageFormat;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
 import java.util.regex.Pattern;
-
-final class TextChecker {
-    static class Arguments {
-        String key;
-        String regexp;
-        String feedback;
-        int flags = Pattern.CASE_INSENSITIVE;
-    }
-
-    private static final class Checker implements Predicate<String> {
-        final Pattern expected;
-        final String feedback;
-
-        Checker(final Pattern expected, final String feedback) {
-            this.expected = expected;
-            this.feedback = feedback;
-        }
-
-        @Override
-        public boolean test(String actual) {
-            return expected.matcher(actual).find();
-        }
-
-        String getFeedback(Object... args) {
-            return MessageFormat.format(feedback, args);
-        }
-    }
-
-    private final Map<String, Checker> map = new HashMap<>();
-
-    public TextChecker add(Consumer<Arguments> builderFunction) {
-        final var args = new Arguments();
-        builderFunction.accept(args);
-        map.put(args.key, new Checker(Pattern.compile(args.regexp, args.flags), args.feedback));
-        return this;
-    }
-
-    public void check(String key, String actual, Object... args) {
-        final var checker = map.get(key);
-        if (checker.test(actual)) {
-            return;
-        }
-        throw new WrongAnswer(checker.getFeedback(args));
-    }
-}
 
 public class NumbersTest extends StageTest {
 
-    private static final TextChecker checker = new TextChecker()
-            .add($ -> {
-                $.key = "not-natural";
-                $.regexp = "(this|the) number is( not|n't) natural";
-                $.feedback = "Number {0} is not natural. Expected error message.";
-            })
-            .add($ -> {
-                $.key = "is-buzz-number";
-                $.regexp = "is(?: a| the)? buzz( number)?";
-                $.feedback = "Not found message that {0} is a Buzz number.";
-            })
-            .add($ -> {
-                $.key = "is-not-buzz-number";
-                $.regexp = "is( not|n't)(?: a| the)? buzz( number)?";
-                $.feedback = "Not found message that {0} is not a Buzz number.";
-            })
-            .add($ -> {
-                $.key = "is-divisible";
-                $.regexp = "is divisible by";
-                $.feedback = "Not found message that {0} is divisible by 7";
-                $.flags += Pattern.LITERAL;
-            })
-            .add($ -> {
-                $.key = "is-ends-with";
-                $.regexp = "(is|it) ends with";
-                $.feedback = "Not found message that {0} is ends with 7";
-            })
-            .add($ -> {
-                $.key = "is-neither";
-                $.regexp = "is neither divisible by 7 nor it ends with 7";
-                $.feedback = "Not found message that {0} is is neither divisible by 7 nor it ends with 7";
-                $.flags += Pattern.LITERAL;
-            })
-            .add($ -> {
-                $.key = "enter-number";
-                $.regexp = "natural number";
-                $.feedback = "The program should ask for a natural number.";
-                $.flags += Pattern.LITERAL;
-            });
+    private enum Key {ENTER_NUMBER, BUZZ_NUMBER}
 
+    private static final TextChecker checker = new TextChecker();
 
     private final long[] number = {1, 2, 3, 4, 5, 9_223_372_036_854_775_807L};
 
     @DynamicTest(data = "number", order = 1)
     CheckResult simpleTest(final long number) {
-        final var program = new TestedProgram();
-
-        checker.check("enter-number", program.start());
-
         final var expected = number % 2 == 0 ? "even" : "odd";
-        final var actual = program.execute(String.valueOf(number)).toLowerCase();
-
-        assertTrue(actual.contains(expected),
-                "Number {0} should be {1}.", number, expected);
-
-        assertTrue(program.isFinished(),
-                "Program should finish after calculating parity of the number.");
-
-        return CheckResult.correct();
+        return checker.start()
+                .check($ -> {
+                    $.key = Key.ENTER_NUMBER;
+                    $.regexp = "natural number";
+                    $.feedback = "The program should ask for a natural number.";
+                    $.flags += Pattern.LITERAL;
+                })
+                .execute(number)
+                .contains(expected, "Number {0} should be {1}.")
+                .finish()
+                .correct();
     }
 
-    private final long[] incorrect = {0, -1, -2, -3, -4, -5};
+    private final long[] notNaturalNumbers = {0, -1, -2, -3, -4, -5};
 
-    @DynamicTest(data = "incorrect", order = 10)
-    CheckResult incorrectNumbers(final long number) {
-        final var program = new TestedProgram();
-
-        checker.check("enter-number", program.start());
-
-        final var actual = program.execute(String.valueOf(number));
-
-        checker.check("not-natural", actual, number);
-
-        assertTrue(program.isFinished(),
-                "Program should finish after printing the error message.");
-
-        return CheckResult.correct();
+    @DynamicTest(data = "notNaturalNumbers", order = 10)
+    CheckResult notNaturalNumbersTest(final long number) {
+        return checker.start()
+                .check(Key.ENTER_NUMBER)
+                .execute(number)
+                .check($ -> {
+                    $.regexp = "(this|the) number is( not|n't) natural";
+                    $.feedback = "Number {0} is not natural. Expected error message.";
+                })
+                .finish()
+                .correct();
     }
 
     private final long[] divisible = {7, 14, 21, 28, 35, 123 * 7, 578 * 7, 9865 * 7};
 
     @DynamicTest(data = "divisible", order = 20)
     CheckResult divisibleNumbers(final long number) {
-        final var program = new TestedProgram();
-
-        checker.check("enter-number", program.start());
-
-        final var actual = program.execute(String.valueOf(number));
-
-        checker.check("is-buzz-number", actual, number);
-        checker.check("is-divisible", actual, number);
-
-        assertTrue(program.isFinished(),
-                "Program should finish after printing the error message.");
-
-        return CheckResult.correct();
+        return checker.start()
+                .check(Key.ENTER_NUMBER)
+                .execute(number)
+                .check($ -> {
+                    $.key = Key.BUZZ_NUMBER;
+                    $.regexp = "is(?: a| the)? buzz( number)?";
+                    $.feedback = "Not found message that {0} is a Buzz number.";
+                })
+                .contains("explanation", "The output should contains explanation.")
+                .contains("is divisible by", "Not found message that {0} {1} 7")
+                .finish()
+                .correct();
     }
 
     private final long[] endsWith7 = {17, 27, 97, 107, 308687, 9821007, 204782607};
 
     @DynamicTest(data = "endsWith7", order = 30)
     CheckResult endsWith7Numbers(final long number) {
-        final var program = new TestedProgram();
-
-        checker.check("enter-number", program.start());
-
-        final var actual = program.execute(String.valueOf(number));
-
-        checker.check("is-buzz-number", actual, number);
-        checker.check("is-ends-with", actual, number);
-
-        assertTrue(program.isFinished(),
-                "Program should finish after printing the error message.");
-
-        return CheckResult.correct();
+        return checker.start()
+                .check(Key.ENTER_NUMBER)
+                .execute(number)
+                .check(Key.BUZZ_NUMBER)
+                .contains("explanation", "The output should contains explanation.")
+                .contains("ends with", "Not found message that {0} {1} 7")
+                .finish()
+                .correct();
     }
 
     private final long[] notBuzz = {6, 11, 18, 29, 34};
 
     @DynamicTest(data = "notBuzz", order = 30)
     CheckResult notBuzzNumbers(final long number) {
-        final var program = new TestedProgram();
-
-        checker.check("enter-number", program.start());
-
-        final var actual = program.execute(String.valueOf(number));
-
-        checker.check("is-not-buzz-number", actual, number);
-        checker.check("is-neither", actual, number);
-
-        assertTrue(program.isFinished(),
-                "Program should finish after printing the error message.");
-
-        return CheckResult.correct();
-    }
-
-    private static void assertTrue(final boolean condition, final String error, final Object... args) {
-        if (!condition) {
-            final var feedback = MessageFormat.format(error, args);
-            throw new WrongAnswer(feedback);
-        }
+        return checker.start()
+                .check(Key.ENTER_NUMBER)
+                .execute(number)
+                .check($ -> {
+                    $.regexp = "is( not|n't)(?: a| the)? buzz( number)?";
+                    $.feedback = "Not found message that {0} is not a Buzz number.";
+                })
+                .contains("explanation", "The output should contains explanation.")
+                .check($ -> {
+                    $.regexp = "is neither divisible by 7 nor it ends with 7";
+                    $.feedback = "Not found message that {0} is not a Buzz number.";
+                })
+                .contains("is neither divisible by 7 nor it ends with 7", "Not found message that {0} {1}")
+                .finish()
+                .correct();
     }
 
 }
---
+++ Amazing Numbers I/task2/test/util/TextChecker.java
@@ -0,0 +1,109 @@
+package util;
+
+import org.hyperskill.hstest.exception.outcomes.WrongAnswer;
+import org.hyperskill.hstest.testcase.CheckResult;
+import org.hyperskill.hstest.testing.TestedProgram;
+
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+public final class TextChecker {
+    public static class Arguments {
+        public Object key;
+        public String regexp;
+        public String feedback;
+        public int flags = Pattern.CASE_INSENSITIVE;
+    }
+
+    private static final class Checker implements Predicate<String> {
+        final Pattern expected;
+        final String feedback;
+
+        Checker(final Pattern expected, final String feedback) {
+            this.expected = expected;
+            this.feedback = feedback;
+        }
+
+        @Override
+        public boolean test(String actual) {
+            return expected.matcher(actual).find();
+        }
+
+        String getFeedback(Object... args) {
+            return MessageFormat.format(feedback, args);
+        }
+    }
+
+    private final Map<Object, Checker> map = new HashMap<>();
+    private TestedProgram program;
+    private String output;
+    private String userInput = "";
+
+    public TextChecker start(String... args) {
+        program = new TestedProgram();
+        output = program.start(args);
+        return this;
+    }
+
+    public TextChecker check(Object key) {
+        final var checker = map.get(key);
+        if (checker.test(output)) {
+            return this;
+        }
+        throw new WrongAnswer(checker.getFeedback(userInput, output));
+    }
+
+    public TextChecker check(Consumer<Arguments> builderFunction) {
+        final var args = new Arguments();
+        builderFunction.accept(args);
+        final var checker = new Checker(Pattern.compile(args.regexp, args.flags), args.feedback);
+        if (Objects.nonNull(args.key)) {
+            map.put(args.key, checker);
+        }
+        if (checker.test(output)) {
+            return this;
+        }
+        throw new WrongAnswer(checker.getFeedback(userInput, output));
+    }
+
+    public CheckResult correct() {
+        return CheckResult.correct();
+    }
+
+    public TextChecker execute(long number) {
+        return execute(String.valueOf(number));
+    }
+
+    public TextChecker execute(String userInput) {
+        this.userInput = userInput;
+        output = program.execute(userInput);
+        return this;
+    }
+
+    public TextChecker contains(String expected, String error) {
+        if (output.toLowerCase().contains(expected)) {
+            return this;
+        }
+        throw new WrongAnswer(MessageFormat.format(error, userInput, expected));
+    }
+
+    public TextChecker finish() {
+        if (program.isFinished()) {
+            return this;
+        }
+        throw new WrongAnswer("Program should finish.");
+    }
+
+    public TextChecker add(Consumer<Arguments> builderFunction) {
+        final var args = new Arguments();
+        builderFunction.accept(args);
+        map.put(args.key, new Checker(Pattern.compile(args.regexp, args.flags), args.feedback));
+        return this;
+    }
+
+}
