80a68ed4ea88da74101f621a01ee8d56254dca0d
Jegors Cemisovs <jegors.cemisovs@sap.com>
2021-04-24 22:12:47 +0300

    Updated implementation for stage 4

--- Amazing Numbers I/task5/src/numbers/Main.java
+++ Amazing Numbers I/task5/src/numbers/Main.java
@@ -7,16 +7,24 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("Welcome to Amazing Numbers!");
+        printHelp();
+
         while (true) {
             System.out.printf("%nEnter a natural number: ");
             final var data = scanner.nextLine().split(" ");
             System.out.println();
+            if (data[0].isBlank()) {
+                printHelp();
+                continue;
+            }
             final var start = getNaturalNumber(data[0]);
             if (start == 0) {
                 break;
             }
             if (start < 0) {
                 System.out.println("This number is not natural!");
+                printHelp();
                 continue;
             }
             if (data.length == 1) {
@@ -26,6 +34,7 @@ public class Main {
             final var count = getNaturalNumber(data[1]);
             if (count < 1) {
                 System.out.println("The count should be natural number.");
+                printHelp();
                 continue;
             }
             LongStream.range(start, start + count).forEach(Main::printProperties);
@@ -33,10 +42,6 @@ public class Main {
     }
 
     private static long getNaturalNumber(final String input) {
-//        if (input.isBlank() || !input.chars().allMatch(Character::isDigit)) {
-//            return -1;
-//        }
-
         if (input.isBlank()) {
             return -1;
         }
@@ -50,5 +55,15 @@ public class Main {
 
     private static void printProperties(long number) {
         System.out.printf("%,16d is %s%n", number, NumberProperties.shortProperties(number));
+    }
+
+
+    private static void printHelp() {
+        System.out.println("Supported requests:");
+        System.out.println("- one natural number to print its properties;");
+        System.out.println("- two natural numbers separated by space:");
+        System.out.println("  - a starting number for the list;");
+        System.out.println("  - a count of numbers in the list;");
+        System.out.println("- 0 for exit. ");
     }
 }
---
+++ Amazing Numbers I/task5/test/NumbersTest.java
@@ -0,0 +1,136 @@
+import org.hyperskill.hstest.dynamic.DynamicTest;
+import org.hyperskill.hstest.exception.outcomes.WrongAnswer;
+import org.hyperskill.hstest.stage.StageTest;
+import org.hyperskill.hstest.testcase.CheckResult;
+import util.NumberProperties;
+import util.TextChecker;
+
+import java.text.MessageFormat;
+import java.util.Random;
+import java.util.regex.Pattern;
+import java.util.stream.LongStream;
+
+public class NumbersTest extends StageTest {
+    private static final Random random = new Random();
+    private static final long RANDOM_NUMBERS_TESTS = 20;
+    private static final long TEST_FIRST_NUMBERS = 20;
+    private static final long MAX_NUMBER = Long.MAX_VALUE;
+
+    private enum Key {HELP, ENTER_NUMBER, NOT_NATURAL, PROPERTIES}
+
+    private final TextChecker checker = new TextChecker()
+            .add($ -> {
+                $.key = Key.HELP;
+                $.regexp = "supported requests";
+                $.feedback = "The program should display an instruction for the user";
+                $.flags += Pattern.LITERAL;
+            }).add($ -> {
+                $.key = Key.ENTER_NUMBER;
+                $.regexp = "natural number";
+                $.feedback = "The program should ask for a natural number.";
+                $.flags += Pattern.LITERAL;
+            }).add($ -> {
+                $.key = Key.NOT_NATURAL;
+                $.regexp = "(this|the) number is( not|n't) natural";
+                $.feedback = "Number {0} is not natural. Expected error message.";
+            }).add($ -> {
+                $.key = Key.PROPERTIES;
+                $.regexp = "properties of ";
+                $.feedback = "The first line of number''s properties should contains \"{1}\".";
+                $.flags += Pattern.LITERAL;
+            });
+
+    private final long[] notNaturalNumbers = {-1, -2, -3, -4, -5};
+
+    @DynamicTest(data = "notNaturalNumbers", order = 10)
+    CheckResult notNaturalNumbersTest(final long number) {
+        return checker.start()
+                .check(Key.HELP)
+                .check(Key.ENTER_NUMBER)
+                .execute(number)
+                .check(Key.NOT_NATURAL)
+                .check(Key.HELP)
+                .check(Key.ENTER_NUMBER)
+                .execute(0)
+                .finished()
+                .correct();
+    }
+
+    @DynamicTest(order = 20)
+    CheckResult finishByZeroTest() {
+        return checker.start()
+                .check(Key.HELP)
+                .check(Key.ENTER_NUMBER)
+                .execute(-5)
+                .check(Key.NOT_NATURAL)
+                .check(Key.HELP)
+                .execute(-7635)
+                .check(Key.NOT_NATURAL)
+                .check(Key.HELP)
+                .check(Key.ENTER_NUMBER)
+                .execute(0)
+                .finished()
+                .correct();
+    }
+
+    @DynamicTest(order = 30)
+    CheckResult oneNumberTest() {
+        checker.start();
+        final var numbers = LongStream.concat(
+                LongStream.range(1, TEST_FIRST_NUMBERS),
+                random.longs(RANDOM_NUMBERS_TESTS, 1, MAX_NUMBER));
+
+        numbers.forEach(number -> {
+            checker.check(Key.ENTER_NUMBER).execute(number).check(Key.PROPERTIES);
+
+            for (var property : NumberProperties.values()) {
+                final var name = property.name().toLowerCase();
+                checker.contains(name, "The property {1} wasn''t found for number {0}.");
+
+                final var expected = property.test(number);
+                final var actual = Boolean.parseBoolean(property.extractValue(checker.getOutput())
+                        .orElseThrow(() -> new WrongAnswer(
+                                "The value for property " + name + " was not found.")));
+
+                if (expected != actual) {
+                    throw new WrongAnswer(MessageFormat.format(
+                            "For property {0} the expected value is {1} but found {2}.",
+                            name, expected, actual));
+                }
+            }
+        });
+        return checker.execute(0).finished().correct();
+    }
+
+    @DynamicTest(order = 40)
+    CheckResult twoNumbersTest() {
+        checker.start()
+                .check(Key.ENTER_NUMBER)
+                .execute("1 " + TEST_FIRST_NUMBERS);
+
+        final var numbers = LongStream.concat(
+                LongStream.range(1, TEST_FIRST_NUMBERS),
+                random.longs(RANDOM_NUMBERS_TESTS, 1, MAX_NUMBER));
+
+        numbers.forEach(number -> {
+            checker.check(Key.ENTER_NUMBER).execute(number).check(Key.PROPERTIES);
+
+            for (var property : NumberProperties.values()) {
+                final var name = property.name().toLowerCase();
+                checker.contains(name, "The property {1} wasn''t found for number {0}.");
+
+                final var expected = property.test(number);
+                final var actual = Boolean.parseBoolean(property.extractValue(checker.getOutput())
+                        .orElseThrow(() -> new WrongAnswer(
+                                "The value for property " + name + " was not found.")));
+
+                if (expected != actual) {
+                    throw new WrongAnswer(MessageFormat.format(
+                            "For property {0} the expected value is {1} but found {2}.",
+                            name, expected, actual));
+                }
+            }
+        });
+        return checker.execute(0).finished().correct();
+    }
+}
