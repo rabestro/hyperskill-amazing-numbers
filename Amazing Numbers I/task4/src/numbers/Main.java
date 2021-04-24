4bcb58415298ec91fedff41a62e0ee946be894b7
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-04-24 21:55:32 +0300

    Updated implementation for stage 4

--- Amazing Numbers I/task4/src/numbers/Main.java
+++ Amazing Numbers I/task4/src/numbers/Main.java
@@ -6,25 +6,34 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        help();
+        printHelp();
         while (true) {
-            System.out.println("Enter a natural number:");
-            NumberProperties.number = scanner.nextLong();
-            if (NumberProperties.number == 0) {
+            System.out.print("Enter a natural number:");
+            final var number = scanner.nextLong();
+            System.out.println();
+            if (number == 0) {
                 break;
             }
-            if (NumberProperties.isNatural()) {
-                System.out.printf("Properties of %,d%n", NumberProperties.number);
-                NumberProperties.stream().forEach(System.out::print);
-            } else {
+            if (number < 0) {
                 System.out.println("This number is not natural!");
-                help();
+                printHelp();
+                continue;
             }
-
+            printProperties(number);
         }
     }
 
-    private static void help() {
+    private static void printProperties(long number) {
+        System.out.printf("Properties of %,d%n", number);
+        for (var property : NumberProperties.values()) {
+            final var name = property.name().toLowerCase();
+            System.out.printf("%12s: %s%n", name, property.test(number));
+        }
+    }
+
+    private static void printHelp() {
         System.out.println("Supported requests:");
+        System.out.println("- a natural number to print its properties ");
+        System.out.println("- 0 for exit.");
     }
 }
--- Amazing Numbers I/task4/src/numbers/NumberProperties.java
+++ Amazing Numbers I/task4/src/numbers/NumberProperties.java
@@ -1,39 +1,26 @@
 package numbers;
 
-import java.util.Arrays;
-import java.util.function.LongFunction;
-import java.util.stream.Stream;
+import java.util.function.LongPredicate;
 
-import static java.lang.Character.getNumericValue;
-
-public enum NumberProperties {
-    PARITY(number -> number % 2 == 0 ? "even" : "odd"),
+public enum NumberProperties implements LongPredicate {
     EVEN(number -> number % 2 == 0),
     ODD(number -> number % 2 != 0),
     BUZZ(number -> number % 7 == 0 || number % 10 == 7),
     DUCK(number -> String.valueOf(number).indexOf('0') != -1),
-    GAPFUL(number -> number > 100
-            && number % (getNumericValue(String.valueOf(number).charAt(0)) * 10L + number % 10) == 0);
+    PALINDROMIC(number -> {
+        final var digits = String.valueOf(number);
+        return new StringBuilder(digits).reverse().toString().equals(digits);
+    });
 
-    public static String FORMAT = "%12s: %s%n";
-    public static long number = 0;
+    private final LongPredicate hasProperty;
 
-    private final LongFunction<Object> calculateProperty;
-
-    NumberProperties(LongFunction<Object> calculateProperty) {
-        this.calculateProperty = calculateProperty;
-    }
-
-    public static boolean isNatural() {
-        return number > 0;
-    }
-
-    public static Stream<NumberProperties> stream() {
-        return Arrays.stream(NumberProperties.values());
+    NumberProperties(LongPredicate calculateProperty) {
+        this.hasProperty = calculateProperty;
     }
 
     @Override
-    public String toString() {
-        return String.format(FORMAT, name().toLowerCase(), calculateProperty.apply(number));
+    public boolean test(long number) {
+        return hasProperty.test(number);
     }
+
 }
